82d33641ede7365bc26d566892167aa211b38fef
atharvagadkari05 <atharvagadkari0512@gmail.com>
2022-01-17 20:09:41 +0530

    client side added

---
+++ clientapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import Register from './pages/register';
+
+function App() {
+  return (
+    <div className="App">
+    <Register/>
+    </div>
+  );
+}
+
+export default App;
---
+++ clientapp/src/pages/register.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+
+function Register() {
+
+
+	const [name, setName] = useState('')
+	const [password, setPassword] = useState('')
+
+	async function registerUser(event) {
+		event.preventDefault()
+
+		const response = await fetch('http://localhost:1337/api/register', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				name,
+				password,
+			}),
+		})
+
+		const data = await response.json()
+		console.log(data)
+
+	}
+
+	return (
+		<div>
+			<h1>Register</h1>
+			<form onSubmit={registerUser}>
+				<input
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					type="text"
+					placeholder="Name"
+				/>
+				<br />
+	
+				
+				<input
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					type="password"
+					placeholder="Password"
+				/>
+				<br />
+				<input type="submit" value="Register" />
+			</form>
+		</div>
+	)
+}
+
+export default Register
