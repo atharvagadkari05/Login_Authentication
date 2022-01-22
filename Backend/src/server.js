4410f4d11431a2cbc0bcf43ebe18a7b30b061a56
Atharva Gadkari <74293435+atharvagadkari05@users.noreply.github.com>
2022-01-22 12:19:03 +0530

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,64 +1,96 @@
-const express = require("express");
-const bcrypt = require("bcryptjs");
+
+
+
+const express = require('express')
+const app = express()
 const cors = require('cors')
-const swal = require("sweetAlert");
-const path = require("path");
-const env = require("dotenv").config();
-const jwt = require("jsonwebtoken");
-const mongoose = require("mongoose");
-const User = require("./model/user");
+const mongoose = require('mongoose')
+const User = require('./models/user.model')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-// Mongoose Connection
-mongoose.connect("mongodb://localhost:27017/loginauth", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true, // these statement are used to get rid of the warnings while starting server.
-});
-
-// Express Connection
-const app = express();
 app.use(cors())
+app.use(express.json())
 
-app.use(express.json());
+mongoose.connect('mongodb://localhost:27017/full-mern-stack-video')
 
-app.post("/api/register", async (req, res) => {
-  try{
-    console.log(req.body)
-  const doc =   await User.create({
-      name:req.body.name,
-      password:req.body.password
-    })
-   await doc.save()
-    
-    
-  res.json({
-    "status": "ok"
+app.post('/api/register', async (req, res) => {
+	console.log(req.body)
+	try {
+		const newPassword = await bcrypt.hash(req.body.password, 10)
+		await User.create({
+			name: req.body.name,
+			email: req.body.email,
+			password: newPassword,
+		})
+		res.json({ status: 'ok' })
+	} catch (err) {
+		res.json({ status: 'error', error: 'Duplicate email' })
+	}
 })
 
-}catch(err){
-res.json({
-  "status":"error", 
-  "error": "Name already exists",
+app.post('/api/login', async (req, res) => {
+	const user = await User.findOne({
+		email: req.body.email,
+	})
+
+	if (!user) {
+		return { status: 'error', error: 'Invalid login' }
+	}
+
+	const isPasswordValid = await bcrypt.compare(
+		req.body.password,
+		user.password
+	)
+
+	if (isPasswordValid) {
+		const token = jwt.sign(
+			{
+				name: user.name,
+				email: user.email,
+			},
+			'secret123'
+		)
+
+		return res.json({ status: 'ok', user: token })
+	} else {
+		return res.json({ status: 'error', user: false })
+	}
 })
-}
- 
-});
 
-app.post("/api/login", async (req, res) => {
-  const { username, password } = req.body;
+app.get('/api/quote', async (req, res) => {
+	const token = req.headers['x-access-token']
 
-  const user = User.findOne(username);
+	try {
+		const decoded = jwt.verify(token, 'secret123')
+		const email = decoded.email
+		const user = await User.findOne({ email: email })
 
-  if (!user) {
-    return res.json({ "status": "ok" });
-  }else{
-    return res.json({ "status": "error", "error": "User Not found" });
+		return res.json({ status: 'ok', quote: user.quote })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: 'invalid token' })
+	}
+})
 
-  }
+app.post('/api/quote', async (req, res) => {
+	const token = req.headers['x-access-token']
 
-  
+	try {
+		const decoded = jwt.verify(token, 'secret123')
+		const email = decoded.email
+		await User.updateOne(
+			{ email: email },
+			{ $set: { quote: req.body.quote } }
+		)
 
-});
+		return res.json({ status: 'ok' })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: 'invalid token' })
+	}
+})
 
-app.listen(4000, () => {
-  console.log("Listening at 4000");
-});
+app.listen(1337, () => {
+	console.log('Server started on 1337')
+})
