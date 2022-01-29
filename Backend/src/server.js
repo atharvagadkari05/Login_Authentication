c9b833d97f830547713ee7cbbf6c6127660742c1
Atharva Gadkari <74293435+atharvagadkari05@users.noreply.github.com>
2022-01-29 12:59:47 +0530

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -20,18 +20,17 @@ app.post('/api/register', async (req, res) => {
 		const newPassword = await bcrypt.hash(req.body.password, 10)
 		await User.create({
 			name: req.body.name,
-			email: req.body.email,
 			password: newPassword,
 		})
 		res.json({ status: 'ok' })
 	} catch (err) {
-		res.json({ status: 'error', error: 'Duplicate email' })
+		res.json({ status: 'error', error: 'Name already exists' })
 	}
 })
 
 app.post('/api/login', async (req, res) => {
 	const user = await User.findOne({
-		email: req.body.email,
+		name: req.body.name,
 	})
 
 	if (!user) {
@@ -58,38 +57,7 @@ app.post('/api/login', async (req, res) => {
 	}
 })
 
-app.get('/api/quote', async (req, res) => {
-	const token = req.headers['x-access-token']
 
-	try {
-		const decoded = jwt.verify(token, 'secret123')
-		const email = decoded.email
-		const user = await User.findOne({ email: email })
-
-		return res.json({ status: 'ok', quote: user.quote })
-	} catch (error) {
-		console.log(error)
-		res.json({ status: 'error', error: 'invalid token' })
-	}
-})
-
-app.post('/api/quote', async (req, res) => {
-	const token = req.headers['x-access-token']
-
-	try {
-		const decoded = jwt.verify(token, 'secret123')
-		const email = decoded.email
-		await User.updateOne(
-			{ email: email },
-			{ $set: { quote: req.body.quote } }
-		)
-
-		return res.json({ status: 'ok' })
-	} catch (error) {
-		console.log(error)
-		res.json({ status: 'error', error: 'invalid token' })
-	}
-})
 
 app.listen(1337, () => {
 	console.log('Server started on 1337')
